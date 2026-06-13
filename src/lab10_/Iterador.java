dba3c4bf2784731c06f344eeb94fee275b784ee2
Susana Bazan <subaez2810@gmail.com>
2026-06-13 12:10:08 -0500

    feat: Implementacion de Iterator

---
+++ src/lab10_/Iterador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package lab10_;
+
+/**
+ *
+ * @author s
+ */
+public interface Iterador {
+    void primero();
+    boolean tieneSiguiente();
+    Pago siguiente();
+}
---
+++ src/lab10_/IteradorEstado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab10_;
+
+/**
+ *
+ * @author s
+ */
+import java.util.ArrayList;
+import java.util.List;
+public class IteradorEstado {
+    
+}
